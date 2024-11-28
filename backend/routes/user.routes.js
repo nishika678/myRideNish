4bc998cf4923b1bf58f714917080ac22bd0552d0
Gacky1 <rachitavasthi.it25@jecrc.ac.in>
2024-11-28 20:01:22 +0530

    JWT and Essential things updated

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router=express.Router();
+const {body} = require('express-validator');
+const userController = require('../controllers/user-controller');
+
+router.post('/register',[
+    body('email').isEmail().withMessage('Invalid email'),
+    body('fullname.fistname').isLength({min:3}).withMessage('First name must be a 3 character or longer'),
+    body('fullname.lastname').isLength({min:3}).withMessage('Last name must be a 3 character or longer'),
+    body('password').isLength({min:8}).withMessage('Password must be at least 8 characters long')
+],
+    userController.registerUser
+)
+
+
+module.exports = router;
