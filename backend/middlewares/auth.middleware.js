ab1cc22b41c593c9f9ca2e15bef1ba9a1d368710
Gacky1 <rachitavasthi.it25@jecrc.ac.in>
2024-12-10 01:19:08 +0530

    Auth middleware setup

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -2,19 +2,30 @@ const userModel=require('../models/user.model');
 const bcrypt=require('bcrypt');
 const jwt=require('jsonwebtoken');
 
-module.exports.authUser=async function(req,res,next){
-    const token= req.cookies.token || req.headers.authorization.split(' ');
-    // Khudse try || token[0]!== 'Bearer'
-    if(!token || token[0]!== 'Bearer'){
-        return res.status(401).json({error:'Access denied'});
+module.exports.authUser = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
     }
-    try{
-        const decoded=jwt.verify(token,process.env.JWT_SECRET);
-        const user=await userModel.findById(decoded._id);
+
+
+    const isBlacklisted = await blackListTokenModel.findOne({ token: token });
+
+    if (isBlacklisted) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    try {
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await userModel.findById(decoded._id)
 
         req.user = user;
+
         return next();
-    }catch(err){
-        return res.status(401).json({error:'Access denied'});
+
+    } catch (err) {
+        return res.status(401).json({ message: 'Unauthorized' });
     }
 }
