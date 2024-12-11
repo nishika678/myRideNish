dbfcd1790b21c34a01004913aa1534ccdad29078
Gacky1 <rachitavasthi.it25@jecrc.ac.in>
2024-12-11 20:09:39 +0530

    Code refined to resolve the issue

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,49 +2,49 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const userSchema=new mongoose.Schema({
-    fullname:{
-        firstname:{
-            type:String,
-            required:true,
-            minLength:[3,'First Name must be a atleast 3 character or longer']
-        },
-        lastname:{
-            type:String,
-            required:true,
-            minLength:[3,'Last Name must be a atleast 3 character or longer']
-        },
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        match:[/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Invalid Email']
-    },
-    password:{
-        type:String,
-        required:true,
-        minLength:[8,'Password must be a atleast 8 character or longer'],
-        select:false
-    },
-    socketId:{
-        type:String,
-    }
-});
 
-userSchema.methods.generateAuthToken = function(){
-    const token=jwt.sign({_id:this._id},process.env.JWT_SECRET)
+const userSchema = new mongoose.Schema({
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minlength: [ 3, 'First name must be at least 3 characters long' ],
+        },
+        lastname: {
+            type: String,
+            minlength: [ 3, 'Last name must be at least 3 characters long' ],
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [ 5, 'Email must be at least 5 characters long' ],
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    socketId: {
+        type: String,
+    },
+})
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
     return token;
 }
 
-userSchema.methods.comparePassword =async function(password){
+userSchema.methods.comparePassword = async function (password) {
     return await bcrypt.compare(password, this.password);
 }
 
-userSchema.statics.hashPassword = async function(password){
-    return await bcrypt.hash(password,10);
+userSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
 }
 
-const userModel=mongoose.model('user',userSchema);
+const userModel = mongoose.model('user', userSchema);
 
-module.exports=userModel;
+
+module.exports = userModel;
--- backend/services/user.service.js
+++ backend/services/user.service.js
@@ -1,13 +1,14 @@
-const userModel=require('../models/user.model');
+const userModel = require('../models/user.model');
 
-module.exports.createUser =async ({
-    firstname,lastname,email,password
-})=>{
-    if(!firstname || !lastname || !email || !password){
+
+module.exports.createUser = async ({
+    firstname, lastname, email, password
+}) => {
+    if (!firstname || !email || !password) {
         throw new Error('All fields are required');
     }
-    const user=userModel.create({
-        fullname:{
+    const user = userModel.create({
+        fullname: {
             firstname,
             lastname
         },
