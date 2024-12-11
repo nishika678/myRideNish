e3f67f1c871faa490b45354ad69b9bf6675fc82e
Gacky1 <rachitavasthi.it25@jecrc.ac.in>
2024-12-12 01:24:43 +0530

    Captain setup done

--- backend/routes/captain.routes.js
+++ backend/routes/captain.routes.js
@@ -2,6 +2,7 @@ const express= require('express');
 const router = express.Router();
 const {body} = require('express-validator');
 const captainController = require('../controllers/captain.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/register',[
     body('fullname.firstname').isLength({min:3}).withMessage('First name must be at least 3 characters long'),
@@ -14,5 +15,11 @@ router.post('/register',[
 ],
     captainController.registerCaptain
 )
+
+router.post('/login', captainController.loginCaptain);
+
+router.get('/profile',authMiddleware.authCaptain,captainController.getCaptainProfile);
+
+router.get('/logout',authMiddleware.authCaptain,captainController.logoutCaptain);
 
 module.exports = router;
