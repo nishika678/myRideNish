506b96096b893775e65f5c20461580092a465cb6
Gacky1 <rachitavasthi.it25@jecrc.ac.in>
2024-12-13 22:47:35 +0530

    Userlogin initiated

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -2,7 +2,25 @@
 
 const UserLogin = () => {
   return (
-    <div>UserLogin</div>
+    <div className="p-7">
+      <form action="">
+        <h3 className="text-xl mb-2">"What's your email?"</h3>
+        <input 
+        type="email" 
+        required 
+        className="bg-[#eeeeee] mb-7  rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+        placeholder="email@example.com"
+        />
+        <h3 className="text-xl mb-2">And your password?</h3>
+        <input 
+        type="password"
+        className="bg-[#eeeeee] mb-7  rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+        required 
+        placeholder="password" 
+        />
+        <button>Login</button>
+      </form>
+    </div>
   )
 }
 
