1c0d61e9b0012918141de7e50b6d3d5ea082120a
Gacky1 <rachitavasthi.it25@jecrc.ac.in>
2024-12-13 22:35:59 +0530

    Created home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,17 @@
 // import React from 'react'  ## No longer required in React 17
 
+import { Link } from "react-router-dom"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="bg-cover bg-center bg-[url(/assets/RaahiHome.png)] h-screen pt-8 w-full flex justify-between flex-col  bg-red-400">
+      <img className="w-16 ml-8" src="/assets/RaahiLogo.png" alt="" />
+      <div className="bg-white pb-7 py-4 px-4">
+        <h2 className="text-3xl font-bold">Get Started with MyRide</h2>
+        <Link to='/login' className="flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5">Continue</Link>
+      </div>
+
+    </div>
   )
 }
 
