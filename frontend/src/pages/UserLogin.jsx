f63943321fb53e23f58095d7ba19d4aecd2a362b
nishika678 <147605762+nishika678@users.noreply.github.com>
2025-03-30 23:16:35 +0530

    Update UserLogin.jsx

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,14 +1,34 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
+import { useState,useContext } from "react"
+import { Link,useNavigate } from "react-router-dom"
+import {UserDataContext} from '../context/UserContext'
+import {Navigate} from 'react-router-dom'
+import axios from 'axios'
 
 const UserLogin = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [userData, setUserData] = useState({})
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setUserData({email: email, password: password})
+
+  const {user,setUser}=useContext(UserDataContext)
+  const navigate=useNavigate()
+
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const userData={
+      email:email,
+      password:password
+    }
+
+    const response= await axios.post(`${import.meta.env.VITE_BASE_URL}/users/login`,userData)
+     if (response.status===200){
+      const data=response.datasetUser(data.user)
+      localStorage.setItem('token',data.token)
+      navigate('/home')
+     }
+
     setEmail('')
     setPassword('')
   }
